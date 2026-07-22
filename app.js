4964ef47556657ad8bd5495bacbb2e6514244c9e
ergin4545 <usermame5252@gmail.com>
2026-07-22 21:56:33 +0300

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ window.onload = function(){
 
     console.log("ERGİN uygulaması başladı");
 
-    const const connectBtn = document.getElementBy Id("btnConnect") = document.getElementById("connectBtn");
+    const const const connectBtn = document.getElementBy Id("btnConnect") = document.getElementBy Id("btnConnect") = document.getElementById("connectBtn");
     const disconnectBtn = document.getElementById("disconnectBtn");
     const scanBtn = document.getElementById("scanBtn");
 
