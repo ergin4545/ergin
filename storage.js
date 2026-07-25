b6677bccb785ce9a2afe8ea2a905f0d769dc3866
ergin4545 <usermame5252@gmail.com>
2026-07-25 19:03:28 +0300

    Update storage.js

--- storage.js
+++ storage.js
@@ -1,55 +1,16 @@
-// storage.js
-
-const STORAGE_KEY = "ergin_scan_history";
+const STORAGE_KEY = "nico_chat_history_v2";
 
 function saveData(value){
-
-let history = getHistory();
-
-history.push({
-
-value:value,
-time:new Date().toLocaleString()
-
-});
-
-localStorage.setItem(
-STORAGE_KEY,
-JSON.stringify(history)
-);
-
+    let history = getHistory();
+    history.push({ value: value, time: new Date().toLocaleString() });
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(history));
 }
 
 function getHistory(){
-
-let data = localStorage.getItem(STORAGE_KEY);
-
-if(data){
-
-return JSON.parse(data);
-
-}
-
-return [];
-
+    let data = localStorage.getItem(STORAGE_KEY);
+    return data ? JSON.parse(data) : [];
 }
 
 function clearHistory(){
-
-localStorage.removeItem(STORAGE_KEY);
-
-}
-
-function getLastRecord(){
-
-let history = getHistory();
-
-if(history.length===0){
-
-return null;
-
-}
-
-return history[history.length-1];
-
+    localStorage.removeItem(STORAGE_KEY);
 }
