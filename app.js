d85ff22b3872119f615dd1e9d458027be68198bb
ergin4545 <usermame5252@gmail.com>
2026-07-27 21:17:43 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // NICO AI Assistant - app.js
 
-const WORKER_URL = "https://bitter-haze-2503.username5252.workers.dev/";
+const WORKER_URL = "https://bitter-haze-2503.usermame5252.workers.dev/";
 
 const chatBox = document.getElementById("chatBox");
 const userInput = document.getElementById("userInput");
@@ -131,3 +131,4 @@ imageInput.addEventListener("change", ()=>{
 
 
 console.log("NICO aktif. Worker bağlantısı hazır.");
+                
